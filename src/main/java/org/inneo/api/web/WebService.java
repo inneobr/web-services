3d06fccc17e7effef1404f9e8a6e2a74853be644
inneobr <tecnologiaesolucoespr@gmail.com>
2023-08-31 12:44:01 -0300

    feat: tratamento exception

--- src/main/java/org/inneo/api/web/WebService.java
+++ src/main/java/org/inneo/api/web/WebService.java
@@ -1,12 +1,12 @@
 package org.inneo.api.web;
 
-import reactor.core.publisher.Mono;
-
-import org.springframework.http.MediaType;
-import org.springframework.http.HttpMethod;
-import org.inneo.api.exceptions.ObjectDefaultException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import reactor.core.publisher.Mono;
+import org.springframework.http.MediaType;
+import org.springframework.http.HttpMethod;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -40,8 +40,7 @@ public class WebService {
 			HsBrasil weather = previsaoTempo.block();
 			return  weather;
 		}catch(Exception e) {
-			logger.error("hsbrasil recusou-se a fornecer os dados.");
-			throw new ObjectDefaultException("hsbrasil recusou-se a fornecer os dados.", e);
+			throw new NullPointerException("hsbrasil recusou-se a fornecer os dados.");
 		}
 	}
 }
